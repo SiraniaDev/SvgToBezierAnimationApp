ce0d72846829606ed68c33cbe4bf10529bc8e398
Nadia M <siraniadev@gmail.com>
2020-06-09 07:10:40 +0200

    Initial Commit

---
+++ SvgToBezierAnimationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SvgToBezierAnimationApp
+//
+//  Created by Nadia Mettioui on 08/06/2020.
+//  Copyright © 2020 Nadia Mettioui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
